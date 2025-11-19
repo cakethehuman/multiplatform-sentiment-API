finish fixing data leakage

src/Vectorize.py
@@ -3,11 +3,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
 
 class Vectorize:
-    def __init__(self,df ,targetColumn):
-        self.df = df
-        self.targetColumn = targetColumn
+    def __init__(self,x_train ,x_test, App):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.App = App
     
     def transform(self):
         tf = TfidfVectorizer(stop_words= 'english')
-        hasil = tf.fit_transform(self.df[self.targetColumn])
-        return hasil
+        X_train = tf.fit_transform(self.x_train)
+        X_test = tf.transform(self.x_test)
+        
+        joblib.dump(X_train, f'Data\interim\{self.App}\X_train.pkl')
+        joblib.dump(X_test, f'Data\interim\{self.App}\X_test.pkl')
+        joblib.dump(tf, "word vectors\Vectorizer.pkl")
+        
+        
+        return X_train,X_test