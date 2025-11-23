9378bf6cf1e4bead576b24df0e9ff5b32e9ef749
cakethehuman <willsonputra17@gmail.com>
2025-11-23 10:07:55 +0700

    added a funtion to auto make proccesed data not finished still need to fix

--- src/main.py
+++ src/main.py
@@ -6,34 +6,27 @@ import joblib
 
 import pandas as pd
 
-
-def main():
-    # Insert data
-    threads = pd.read_csv(r"Data\raw\threads\threads_reviews.csv")
-    spotify = pd.read_csv(r"Data\raw\spotify\reviews.csv")
-
-    # Dropping unwanted columns
-    threads = threads.drop(['source','review_date'], axis= 1)
-    spotify = spotify.drop(["Time_submitted", 'Total_thumbsup', 'Reply'], axis= 1)
-
-    # Data labeling
-    sp_label = Label(spotify, "Rating", "Spotify")
-    spotify_label = sp_label.sentiment()
-
-    joblib.dump(spotify_label, f'Data\preproccesed\spotify_clean.pkl')
-
-    # 2) Process text (assume review column is called "Review" or similar)
-    sp_processor = Procces(spotify_label, "Review")
-    spotify_processed = sp_processor.proccesdata()
-
-    # 3) dump the cleaned data
-    joblib.dump(spotify_processed, f'Data\preproccesed\spotify_processed.pkl')
+def setup(Data, ratingcolumn, App, textColumn):
     
-    X_spotify = spotify_processed["Review"]
-    y_spotify = spotify_processed["Sentiment"]
-
-    # 6) Train-test split + save
-    splitter = Split(X_spotify, y_spotify, App = "spotify")
+    # 1. Label the data
+    label = Label(Data, ratingcolumn, App)
+    hasillabel = label.sentiment()
+    
+    # Dump the data for anlize
+    joblib.dump(hasillabel, f'Data\preproccesed\{App}\{App}_clean.pkl')
+    
+    # 2. Preprocess data
+    proccesdata = Procces(hasillabel, textColumn)
+    hasilprocces = proccesdata.proccesdata()
+    
+    # 3. dump data
+    
+    joblib.dump(hasilprocces, f"Data\preproccesed\{App}\{App}_proccesed.pkl")
+    
+    X = hasilprocces[textColumn]
+    y = hasilprocces["Sentiment"]
+    
+    splitter = Split(X, y, App)
     X_train, X_test, y_train, y_test = splitter.tts()
     
     print("X_train:", X_train.shape)
@@ -41,13 +34,62 @@ def main():
     print("y_train:", y_train.shape)
     print("y_test: ", y_test.shape)
     
-    Vtr = Vectorize(x_train = X_train, x_test = X_test, App = "spotify")
+    Vtr = Vectorize(X_train,X_test, App)
     X_train_vec,X_test_vec = Vtr.transform()
     
     print(f"hasil vectorize train: {X_train_vec}" )
     print(f"hasil vectorize test: {X_test_vec}" )
 
 
+def main():
+    
+    # Insert data
+    threads = pd.read_csv(r"Data\raw\threads\threads_reviews.csv")
+    spotify = pd.read_csv(r"Data\raw\spotify\reviews.csv")
+    
+    setup(threads, "Rating", "Spotify", "Review")
+
 
 if __name__ == "__main__":
     main()
+    
+    
+    
+    # # Insert data
+    # threads = pd.read_csv(r"Data\raw\threads\threads_reviews.csv")
+    # spotify = pd.read_csv(r"Data\raw\spotify\reviews.csv")
+
+    # # Dropping unwanted columns
+    # threads = threads.drop(['source','review_date'], axis= 1)
+    # spotify = spotify.drop(["Time_submitted", 'Total_thumbsup', 'Reply'], axis= 1)
+
+    # # Data labeling
+    # sp_label = Label(spotify, "Rating", "Spotify")
+    # spotify_label = sp_label.sentiment()
+
+    # joblib.dump(spotify_label, f'Data\preproccesed\spotify_clean.pkl')
+
+    # # 2) Process text (assume review column is called "Review" or similar)
+    # sp_processor = Procces(spotify_label, "Review")
+    # spotify_processed = sp_processor.proccesdata()
+
+    # # 3) dump the cleaned data
+    # joblib.dump(spotify_processed, f'Data\preproccesed\spotify_processed.pkl')
+    
+    # X_spotify = spotify_processed["Review"]
+    # y_spotify = spotify_processed["Sentiment"]
+
+    # # 6) Train-test split + save
+    # splitter = Split(X_spotify, y_spotify, App = "spotify")
+    # X_train, X_test, y_train, y_test = splitter.tts()
+    
+    # print("X_train:", X_train.shape)
+    # print("X_test: ", X_test.shape)
+    # print("y_train:", y_train.shape)
+    # print("y_test: ", y_test.shape)
+    
+    # Vtr = Vectorize(x_train = X_train, x_test = X_test, App = "spotify")
+    # X_train_vec,X_test_vec = Vtr.transform()
+    
+    # print(f"hasil vectorize train: {X_train_vec}" )
+    # print(f"hasil vectorize test: {X_test_vec}" )
